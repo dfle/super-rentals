09052e5b5e3ba39be06f0e5d28475f012ef582fe
dfle <diannefle@gmail.com>
2024-11-12 13:27:17 -0800

    style: run lint

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,7 +6,7 @@ export default class IndexRoute extends Route {
   async model() {
     let response = await fetch('/api/rentals.json');
     let { data } = await response.json();
-    
+
     return data.map((model) => {
       let { attributes } = model;
       let type;
