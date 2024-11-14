90c3385d06eebccabffe4965624c302c152084e6
dfle <diannefle@gmail.com>
2024-11-14 12:48:26 -0800

    feat: make other attributes searchable

--- app/components/rentals/filter.js
+++ app/components/rentals/filter.js
@@ -5,7 +5,15 @@ export default class RentalsFilter extends Component {
     let { rentals, query } = this.args;
 
     if (query) {
-      rentals = rentals.filter((rental) => rental.title.includes(query));
+      rentals = rentals.filter((rental) => [
+        rental.title,
+        rental.city,
+        rental.category,
+        rental.type,
+        rental.description
+      ].map(s => s.toLowerCase())
+      .join(' ')
+      .includes(query.toLowerCase()));
     }
 
     return rentals;
